42cf81251bb0a06a6cb9ed77347348d29c652899
tyler quinn <tyler.quinn032@gmail.com>
2022-12-21 20:01:26 -0800

    taking a break

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -2,10 +2,18 @@ import Calculator from './../src/galactic.js'
 
 describe('Calculator', () => {
 
-  test('it should return the age from earth and the life expectancy', () => {
-    const calc = new Calculator(24, 100, "earth");
-    expect(calc.age).toEqual(24);
-    expect(calc.lifeSpan).toEqual(100);
-    expect(calc.planet).toEqual("earth");
+  // test('it should return the age from earth and the life expectancy', () => {
+  //   const calc = new Calculator(24, 100, "earth");
+  //   expect(calc.age).toEqual(24);
+  //   expect(calc.lifeSpan).toEqual(100);
+  //   expect(calc.planet).toEqual("earth");
+  // });
+
+  test('it should return mercurys age and life expectancy from earth', () => {
+    let mercury = new Calculator(24, 100, "mercury");
+    expect(mercury.age).toEqual(100);
+    expect(mercury.lifeSpan).toEqual(416.67);
+    expect(mercury.planet).toEqual(mercury);
   });
+
 });
--- src/galactic.js
+++ src/galactic.js
@@ -1,10 +1,10 @@
 export default class Calculator {
-  constructor(age, lifeSpan, planet) {
-    this.age = age;
-    this.planet = planet;
-    this.lifeSpan = lifeSpan;
-    this.lifeLeft;
-  }
+  constructor(age, lifeSpan, planet) //{
+  //   this.age = age;
+  //   this.planet = planet;
+  //   this.lifeSpan = lifeSpan;
+  //   this.lifeLeft;
+  // }
 
   mercuryCalculation() {
     this.age = Math.round(this.age / .24);
