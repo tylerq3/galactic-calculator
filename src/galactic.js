8071b8434209c2a28c22f53e3ce698381c1929c6
tyler quinn <tyler.quinn032@gmail.com>
2022-12-21 18:56:13 -0800

    add calculator function

--- src/galactic.js
+++ src/galactic.js
@@ -1,5 +1,10 @@
-export default function () {
+export default class Calculator {
+  // constructor(age, lifeExpectancy, planet) {
+  //   this.age = age;
+  //   this.planet = planet;
+  //   this.lifeExpectancy = lifeExpectancy;
+  //   this.lifeLeft;
+  }
 
-}
 
 
