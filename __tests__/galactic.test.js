977ad054df83e4e310b18fe829da838944cb2c37
tyler quinn <tyler.quinn032@gmail.com>
2022-12-25 10:54:51 -0800

    bad fail venus

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -2,18 +2,24 @@ import Calculator from './../src/galactic.js'
 
 describe('Calculator', () => {
 
-  // test('it should return the age from earth and the life expectancy', () => {
-  //   const calc = new Calculator(24, 100, "earth");
-  //   expect(calc.age).toEqual(24);
-  //   expect(calc.lifeSpan).toEqual(100);
-  //   expect(calc.planet).toEqual("earth");
-  // });
+  test('it should return the age from earth and the life expectancy', () => {
+  const calc = new Calculator(24, 100, "earth");
+  expect(calc.age).toEqual(24);
+  expect(calc.lifeSpan).toEqual(100);
+  expect(calc.planet).toEqual("earth");
+  });
 
   test('it should return mercurys age and life expectancy from earth', () => {
     let mercury = new Calculator(24, 100, "mercury");
     expect(mercury.age).toEqual(100);
     expect(mercury.lifeSpan).toEqual(416.67);
-    expect(mercury.planet).toEqual(mercury);
+    expect(mercury.planet).toEqual("mercury");
   });
 
+  test('it should return venus age and life expectancy from earth', () => {
+    let venus = new Calculator(24, 100, "venus");
+    expect(venus.age).toEqual(38.70);
+    expect(venus.lifeSpan).toEqual(161.29);
+    expect(venus.planet).toEqual("venus")
+  })
 });
--- src/galactic.js
+++ src/galactic.js
@@ -1,15 +1,20 @@
 export default class Calculator {
-  constructor(age, lifeSpan, planet) //{
-  //   this.age = age;
-  //   this.planet = planet;
-  //   this.lifeSpan = lifeSpan;
-  //   this.lifeLeft;
-  // }
+  constructor(age, lifeSpan, planet) {
+    this.age = age;
+    this.planet = planet;
+    this.lifeSpan = lifeSpan;
+    this.lifeLeft;
+  }
 
-  mercuryCalculation() {
+  mercury() {
     this.age = Math.round(this.age / .24);
     this.lifeSpan = Math.round(this.lifeSpan / .24);
 }
+
+  venus() {
+    this.age = Math.round(this.age / .62);
+    this.lifeSpan = Math.round(this.age / .62);
+  }
 
 }
 
