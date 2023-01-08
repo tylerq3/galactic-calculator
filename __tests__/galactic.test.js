271858df16bec73291cf67e095353263459575d4
tyler quinn <tyler.quinn032@gmail.com>
2023-01-07 20:11:54 -0800

    bad fail venus

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -10,10 +10,29 @@ describe('Calculator', () => {
   });
 
   test('it should return mercurys age and life expectancy from earth', () => {
-    let mercury1 = new Calculator(24, "mercury");
-    mercury1.mercury();
-    expect(mercury1.age).toEqual(100);
+    let mercury = new Calculator(24, "mercury");
+    mercury.mercury();
+    expect(mercury.age).toEqual(100);
   });
 
+  test('it should return venus age and life expectancy from earth', () => {
+  let venus = new Calculator(24, "venus");
+  venus.venus();
+  expect(venus.age).toEqual(38.7);
+  })
+
+  test('it should return mars age and life expectancy from earth', () => {
+    let mars = new Calculator(24, 100, "mars");
+    expect(mars.age).toEqual(12.7);
+    expect(mars.lifeSpan).toEqual(53.1);
+    expect(mars.planet).toEqual("mars");
+  })
+
+  test('it should return jupiters age and life expectancy from earth', () => {
+    let jupiter = new Calculator(24, 100, "mars");
+    expect(jupiter.age).toEqual(2);
+    expect(jupiter.lifeSpan).toEqual(8.4);
+    expect(jupiter.planet).toEqual("jupiter");
+  });
 
 });
